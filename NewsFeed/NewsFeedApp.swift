8983e5d87a5009ade3353620007081aa391b5aa4
Colin Dively <colinblairdively@gmail.com>
2022-09-27 23:23:55 -0400

    Initial Commit

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by Colin Dively on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
